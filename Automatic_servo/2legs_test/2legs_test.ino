be9895a866485acb10ae08b5cee41000ccc9cd23
suraj <surajmahangade99@gmail.com>
2019-03-10 16:52:57 +0530

    2 legs code ,not tested

--- Automatic_servo/2legs_test/2legs_test.ino
+++ Automatic_servo/2legs_test/2legs_test.ino
@@ -10,11 +10,17 @@
 MPU6050 accelgyro(0x69); // <-- use for AD0 high
 int e_mea = 1;
 float angle;
-int e_est = 1; int q = 10 ;
-SimpleKalmanFilter kfx = SimpleKalmanFilter(e_mea, e_est, q);
-SimpleKalmanFilter kfy = SimpleKalmanFilter(e_mea, e_est, q);
-SimpleKalmanFilter kfx1 = SimpleKalmanFilter(e_mea, e_est, q);
-SimpleKalmanFilter kfy1 = SimpleKalmanFilter(e_mea, e_est, q);
+int e_est = 1; 
+int q = 1 ;
+SimpleKalmanFilter kfx11 = SimpleKalmanFilter(e_mea, e_est, q);
+SimpleKalmanFilter kfy11 = SimpleKalmanFilter(e_mea, e_est, q);
+SimpleKalmanFilter kfx12 = SimpleKalmanFilter(e_mea, e_est, q);
+SimpleKalmanFilter kfy12 = SimpleKalmanFilter(e_mea, e_est, q);
+SimpleKalmanFilter kfx21 = SimpleKalmanFilter(e_mea, e_est, q);
+SimpleKalmanFilter kfy21 = SimpleKalmanFilter(e_mea, e_est, q);
+SimpleKalmanFilter kfx22 = SimpleKalmanFilter(e_mea, e_est, q);
+SimpleKalmanFilter kfy22 = SimpleKalmanFilter(e_mea, e_est, q);
+
 //e_mea: Measurement Uncertainty - How much do we expect to our measurement vary
 //e_est: Estimation Uncertainty - Can be initilized with the same value as e_mea since the kalman filter will adjust its value.
 //q: Process Variance - usually a small number between 0.001 and 1 - how fast your measurement moves. Recommended 0.01. Should be tunned to your needs.
@@ -32,7 +38,7 @@ float a2 = 39;
 float a3 = 0;
 float a4 = 39;
 float fb1;
-volatile int  no_pointer=3;
+volatile int  no_pointer = 3;
 int relay[4][4] = {{4, 5 , 6, 7}, {31, 33, 35, 37}, {39, 41, 43, 45}, {47, 49, 51, 53}};//{{23, 25, 27, 29}, {31, 33, 35, 37}, {39, 41, 43, 45}, {47, 49, 51, 53}};
 float T[][4] = {{0, 0}, {0, 0}, {0, 0}, {0, 0}};
 volatile int flag[][4] = {{0, 0}, {0, 0}, {0, 0}, {0, 0}};
@@ -40,7 +46,8 @@ volatile int neg_flag[4] = {0, 0, 0, 0};
 volatile int pos_flag[4] = {0, 0, 0, 0};
 //volatile float points[36][2] = { { 0,55.15}, { 0, 54.15}, { 0, 53.15}, { 0, 52.15}, { 0, 51.15}, { 0, 50.15}, { -6, 51.15}, { -8, 52.15}, { -9.17, 53.15}, { -9.8, 54.15},{-10, 55.15},{ -9, 55.15},{ -8, 55.15},{ -7, 55.15},{ -6, 55.15},{ -5, 55.15},{ -4, 55.15},{ -3, 55.15},{ -2, 55.15},{ -1, 55.15},{ 0, 55.15},{ 1, 55.15},{ 2, 55.15},{ 3, 55.15} ,{ 4, 55.15},{ 5, 55.15},{ 6, 55.15},{ 7, 55.15},{ 8, 55.15},{ 9, 55.15},{ 10, 55.15},{ 9.8, 54.15},{ 9.17, 53.15},{ 8, 52.15},{ 6, 51.15},{ 0, 55.15}};
 //volatile float points[9][2] = {{-19.34,54.90},{-17.3,49.96},{-13.4,45.153},{9.8,42.66},{13.7,45.429},{17.2,49.79},{19.3,54.755},{20,60},{-20,60} };
-volatile float points[3][2] = {{-2,40.41},{20,60},{-20,60} };
+volatile float points_leg1[3][2] = {{ -2, 40.41}, {20, 60}, { -20, 60} };
+volatile float points_leg2[3][2] = {{ -2, 40.41}, {20, 60}, { -20, 60} };
 //volatile float points[10][2] = {/*{0, 55.15}, { 0 , 54.15}, { 0, 53.15}, { 0, 52.15}, { 0, 51.15},*/ { 0, 50.15}, { -6, 51.15}, { -8, 52.15}, { -9.17, 53.15}, { -9.8, 54.15}, { -10, 55.15}, { -9.8, 56.15}, { -9.17, 57.15}, { -8, 58.15}, { -6, 59.15}};
 //volatile float points[36][2] = { { 0, 55.15}, { 0, 54.15}, { 0, 53.15}, { 0, 52.15}, { 0, 51.15}, { 0, 50.15}, { -6, 51.15}, { -8, 52.15}, { -9.17, 53.15}, { -9.8, 54.15},{-10, 55.15},{ -9, 55.15},{-8,55.15},{-7,55.15},{-6,55.15},{-5,55.15},{-4,55.15},{-3,55.15},{-2,55.15},{-1,55.15},{0,55.15},{1,55.15},{2,55.15},{3,55.15},{4,55.15},{5,55.15},{6,55.15},{7,55.15},{8,55.15},{9,55.15},{10,55.15},{9.8,54.15},{9.17,53.15},{8,52.15},{6,51.15},{0.50.15} };
 volatile int pointer = 0;
@@ -49,13 +56,32 @@ volatile int pointer = 0;
 
 class Leg
 {
+    public:
     volatile int leg;  //private variable for leg number
     volatile float X;
     volatile float Y;
-  public:
-    Leg(int _leg)
+    volatile float points[3][2];
+     SimpleKalmanFilter kfx = SimpleKalmanFilter(e_mea, e_est, q);
+     SimpleKalmanFilter kfy = SimpleKalmanFilter(e_mea, e_est, q);
+     SimpleKalmanFilter kfx1 = SimpleKalmanFilter(e_mea, e_est, q);
+     SimpleKalmanFilter kfy1 = SimpleKalmanFilter(e_mea, e_est, q);
+//  
+    Leg(int _leg,SimpleKalmanFilter _kfx,SimpleKalmanFilter _kfy, SimpleKalmanFilter _kfx1,SimpleKalmanFilter _kfy1,volatile float _points[][2])
     {
       leg = _leg; //declare public variable
+      SimpleKalmanFilter kfx = _kfx;
+      SimpleKalmanFilter kfy = _kfy;
+      SimpleKalmanFilter kfx1 = _kfx1;
+      SimpleKalmanFilter kfy1 = _kfy1;
+      for (int i=0;i<3;i++)
+      {
+        for (int j=0;j<2;j++)
+        {
+          points[i][j]=_points[i][j];
+        }
+      }
+      
+      
     }
     //*************************//
     //gotopos takes X and Y and Goes to that position
@@ -244,15 +270,15 @@ class Leg
       //      //Find error
       error1 = T[0][leg] - fb1;
       error2 = T[1][leg] - fb2;
-      int pwm1 = (abs(error1) - abs(error2))*Kp1 + standard;
+      int pwm1 = (abs(error1) - abs(error2)) * Kp1 + standard;
       int pwm2 = 255;
-//      int pwm1 = int((error1) * Kp1 * (abs(error1) - abs(error2)) + standard);
-//      int pwm2 = int((error2) * Kp2 * (pow(1.7, (-abs(error1) + 20)) + 1) + standard);
+      //      int pwm1 = int((error1) * Kp1 * (abs(error1) - abs(error2)) + standard);
+      //      int pwm2 = int((error2) * Kp2 * (pow(1.7, (-abs(error1) + 20)) + 1) + standard);
       pwm1 = constrain(pwm1, 0, 255);
-//      pwm2 = constrain(pwm2, 0, 255);
+      //      pwm2 = constrain(pwm2, 0, 255);
 
 
-      analogWrite(11,100); //pwm1);
+      analogWrite(11, 100); //pwm1);
       analogWrite(3, 255); //);
 
       Serial.println(pwm1);
@@ -261,7 +287,7 @@ class Leg
       if ((error1) <= 5 && (error1) >=  -5 )
       {
         //hardstop(relay[leg][0], relay[leg][1]);
-        analogWrite(11,0);
+        analogWrite(11, 0);
         flag[0][leg] = 0;
         Serial.println("L1 stop");
 
@@ -274,7 +300,7 @@ class Leg
       }
       if ((error2) <= 5 && (error2) >= -5 )
       {
-       analogWrite(3,0);
+        analogWrite(3, 0);
         flag[1][leg] = 0;
         Serial.println("L2 stop");
 
@@ -410,10 +436,11 @@ class Leg
 };
 //*****************************************************************************************************************************//
 
-Leg leg1 = Leg(0);
-Leg leg2 = Leg(1);
-Leg leg3 = Leg(2);
-Leg leg4 = Leg(3);
+Leg leg1 = Leg(0,kfx11,kfy11,kfx12,kfy12,points_leg1);
+Leg leg2 = Leg(0,kfx21,kfy21,kfx22,kfy22,points_leg2);
+ 
+//Leg leg3 = Leg(2);
+//Leg leg4 = Leg(3);
 
 
 //Setup function to setup baud rate pinModes
@@ -435,8 +462,8 @@ void setup()
     float fb2, avg2, angle;
     accelgyro.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
 
-    ax = kfx1.updateEstimate(ax);
-    az = kfy1.updateEstimate(az);
+    ax = leg2.kfx1.updateEstimate(ax);
+    az = leg2.kfy1.updateEstimate(az);
     Serial.print("Leg2   angle=");
     //      Serial.print(ax);
     Serial.print(" ");
@@ -457,8 +484,8 @@ void setup()
     float  avg1, angle;
     accelgyro.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
 
-    ax = kfx.updateEstimate(ax);
-    az = kfy.updateEstimate(az);
+    ax = leg1.kfx.updateEstimate(ax);
+    az = leg1.kfy.updateEstimate(az);
 
     Serial.print("Leg1   angle=");
     //      Serial.print(ax);
