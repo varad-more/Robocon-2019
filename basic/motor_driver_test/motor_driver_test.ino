be56980975c37b90b7bbc0d42a6b38e941c9d12a
dudeiam <prathameshn99@gmail.com>
2019-03-20 18:33:59 +0530

    clibration of bot (not finished)

--- basic/motor_driver_test/motor_driver_test.ino
+++ basic/motor_driver_test/motor_driver_test.ino
@@ -1,12 +1,17 @@
-#define dir 34+1
-#define brk 26+1
-#define pwm 30+1
-
+#define dir 34+1+1+1
+#define brk 26
+#define pwm 30+1+1+1
+#define brk1 27
+#define brk2 28
+#define brk3 29
 
 void setup() {
   // put your setup code here, to run once:
 pinMode(dir,OUTPUT);
 pinMode(brk,OUTPUT);
+pinMode(brk1,OUTPUT);
+pinMode(brk2,OUTPUT);
+pinMode(brk3,OUTPUT);
 pinMode(pwm,OUTPUT);
 }
 
@@ -21,10 +26,12 @@ void loop() {
 //delay(1000);
 //digitalWrite(brk,HIGH);
 //delay (500);
-digitalWrite(brk,LOW);
-digitalWrite(dir,HIGH);
-analogWrite(pwm,255);
-delay (1000);
+digitalWrite(brk,HIGH);
+digitalWrite(brk1,HIGH);
+digitalWrite(brk2,HIGH);
+digitalWrite(brk3,HIGH);
+
+
 }
 
 //int brake[2][2]={{26,27},{28,29}};//checked 
